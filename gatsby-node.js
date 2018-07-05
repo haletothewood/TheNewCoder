fc5ff717d7a17d39588a8f5ce3d21eaa9aaa490a
David Halewood <davidnhalewood@hotmail.com>
2018-07-06 00:01:10 +0100

    WIP: Only create post in db if doesn't exist: hardcoded

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,33 @@ const path = require('path')
 const fetch = require('node-fetch')
 const formurlencoded = require('form-urlencoded').default
 
+const post = async (node) => {
+  const response = await fetch('https://the-new-coder-api.herokuapp.com/posts', {
+    method: 'post',
+    headers: {'Content-Type': "application/x-www-form-urlencoded"},
+    body: formurlencoded({
+      "title": `${node.frontmatter.title}`
+    })
+  })
+  const json = await response
+  console.log(json)
+}
+
+const get = async (node) => {
+  const response = await fetch(`https://the-new-coder-api.herokuapp.com/posts/5b3e9dfd3aeee60014336422`, {
+    method: 'get'
+  })
+  const status = await response.status
+  return status
+}
+
+const sendPostToDb = async (node) => {
+  const response = await get(node)
+  if (response != 200) {
+    post(node)
+   }
+}
+
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
@@ -30,13 +57,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       return Promise.reject(result.errors)
     }
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      fetch('https://the-new-coder-api.herokuapp.com/posts', {
-        method: 'post',
-        headers: {'Content-Type': "application/x-www-form-urlencoded"},
-        body: formurlencoded({
-          "title": `${node.frontmatter.title}`
-        })
-      }).then(res => console.log(res))
+      sendPostToDb(node)
       createPage({
         path: node.frontmatter.path,
         component: blogPostTemplate,
@@ -44,4 +65,15 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       })
     })
   })
+}
+
+exports.modifyWebpackConfig = (config, env) => {
+  if (env === 'build-javascript' || env === 'develop') {
+    const previous = config.resolve().entry
+    config._config.entry = []
+    config.merge({
+      entry: ['babel-polyfill'].concat(previous)
+    })
+  }
+  return config
 }
